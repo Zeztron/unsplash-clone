2c7c806a44021b2e144e0b6ddcd2682f55a40d39
Zeztron <pateltharsh@gmail.com>
2018-11-18 16:44:08 -0500

    added axios and unsplash api to search for pics

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get("https://api.unsplash.com/search/photos", {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization:
+                    "Client-ID 68055bf6f199dedeb726f6e33548dbdc3e68858c741baf581852a0c77f4d309c"
+            }
+        });
     }
     render() {
         return (
