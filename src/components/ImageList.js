e13ad24fbb5435e00445af5fd021afab42860851
Zeztron <pateltharsh@gmail.com>
2018-11-19 10:58:08 -0500

    Added grid system

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
+import "./ImageList.css";
 import React from "react";
 
 const ImageList = props => {
     const images = props.images.map(({description, id, urls}) => {
         return <img key={id} src={urls.regular} alt={description} />;
     });
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
