f62e750844817ce595ccd060e586afbe967e39ed
Zeztron <pateltharsh@gmail.com>
2018-11-19 10:12:29 -0500

    Added binding callbacks

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-    async onSearchSubmit(term) {
+    state = {images: []};
+    onSearchSubmit = async term => {
         const response = await axios.get(
             "https://api.unsplash.com/search/photos",
             {
@@ -17,12 +18,14 @@ class App extends React.Component {
             }
         );
 
-        console.log(response.data.results);
-    }
+        console.log(this);
+        this.setState({images: response.data.results});
+    };
     render() {
         return (
             <div className="ui container" style={{marginTop: "10px"}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
